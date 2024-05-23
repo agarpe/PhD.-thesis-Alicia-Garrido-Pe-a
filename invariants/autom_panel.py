d768a64a3aaf51528c09db238abf31745337f5a1
Alicia  Garrido <aliciagp96@gmail.com>
2024-05-23 11:20:43 +0200

    Scripts to generate panels

---
+++ invariants/autom_panel.py
@@ -0,0 +1,75 @@
+import glob
+import os
+
+
+paths = glob.glob('/home/agarpe/Workspace/tesis/PhD.-thesis-Alicia-Garrido-Pe-a/invariants/data/SUSSEX/*/images/')
+
+print(paths)
+
+
+headers = '''\\documentclass{article}
+\\usepackage{graphicx}
+\\usepackage{overpic}
+
+\\usepackage[left=0.7in,right=0.7in,top=0.1in]{geometry}
+
+
+\\begin{document}\n'''
+
+figure = '''    \\begin{figure}[htbp]
+        \\centering
+        \\begin{minipage}[b]{\\textwidth}
+            \\centering
+            \\includegraphics[width=\\textwidth,height=0.1\\textheight]{_signal_intervals_zoom.pdf}
+            \\includegraphics[width=\\textwidth]{_signal_intervals_cycle.pdf}
+            \\includegraphics[width=\\textwidth]{_time_cycle.pdf}
+        \\end{minipage}
+        \\centering
+        \\begin{minipage}[b]{\\textwidth}
+            \\begin{minipage}[b]{0.45\\textwidth}
+                \\centering
+                \\includegraphics[width=\\textwidth]{_boxplot.pdf}
+            \\end{minipage}
+            \\begin{minipage}[b]{0.53\\textwidth}
+                \\centering
+                \\begin{minipage}[b]{\\textwidth}
+                    \\centering
+                    \\includegraphics[width=\\textwidth]{_durations.pdf}
+                \\end{minipage}\\
+                \\begin{minipage}[b]{\\textwidth}
+                    \\centering
+                    \\includegraphics[width=\\textwidth]{_intervals.pdf}
+                \\end{minipage}\\
+                \\begin{minipage}[b]{\\textwidth}
+                    \\centering
+                    \\includegraphics[width=\\textwidth]{_delays.pdf}
+                \\end{minipage}
+            \\end{minipage}
+        \\end{minipage}
+    \\end{figure}
+\
+\\end{document}'''
+
+
+def create_file(path):
+    print("Creating file ", path+"panel_with_intervals.tex")
+    file_name = path+'panel_with_intervals.tex'
+    with open(file_name,'w') as file:
+        file.write(headers)
+        file.write("\\graphicspath{ { %s } }\n"%path)
+        file.write(figure)
+
+    # print('pdflatex -synctex=interaction=nonstopmode %s'%file_name)
+    os.system('cd %s'%path)
+    os.chdir(path)
+    os.system('pwd')
+    os.system('pdflatex -interaction=nonstopmode panel_with_intervals.tex')
+    os.system('rm panel_with_intervals.log panel_with_intervals.aux')
+
+for path in paths:
+    inner_paths = glob.glob(path+'/*/')
+    if len(inner_paths) > 0:
+        for inner_path in inner_paths:
+            create_file(inner_path)
+    else:
+        create_file(path)
---
+++ invariants/autom_panel_pairplots.py
@@ -0,0 +1,62 @@
+import glob
+import os
+
+
+paths = glob.glob('/home/agarpe/Workspace/tesis/PhD.-thesis-Alicia-Garrido-Pe-a/invariants/data/SUSSEX/*/images/')
+
+print(paths)
+
+
+headers = '''\\documentclass{article}
+\\usepackage{graphicx}
+\\usepackage{overpic}
+
+\\usepackage[left=0.7in,right=0.7in,top=0.1in]{geometry}
+
+
+\\begin{document}\n'''
+
+figure = '''\\begin{figure}[htbp]
+   \\raggedleft
+   \\begin{minipage}[b]{0.85\\textwidth}
+       \\raggedleft
+       \\includegraphics[width=\\textwidth]{_time_cycle.pdf}
+       \\raggedleft
+       \\includegraphics[width=\\textwidth]{_boxplot_h.pdf}     
+   \\end{minipage}
+   \\centering
+   \\begin{minipage}{\\textwidth}
+       \\centering
+       \\begin{overpic}[width=\\textwidth]{_output_pairplot.png} % Base image
+           \\put(50,55){ % Position the second image (70%, 60% of width/height of figure1)
+               \\includegraphics[width=0.4\\textwidth]{_signal_intervals_cycle_stretch.pdf} % The overlapping image
+           }
+       \\end{overpic}
+   \\end{minipage}
+\\end{figure}
+\\end{document}
+'''
+
+
+def create_file(path):
+
+    print(path)
+    print("Creating file ", path+"panel_with_pairplot.tex")
+    file_name = path+'panel_with_pairplot.tex'
+    with open(file_name,'w') as file:
+        file.write(headers)
+        file.write("\\graphicspath{ { %s } }\n"%path)
+        file.write(figure)
+
+    os.chdir(path)
+    os.system('pwd')
+    os.system('pdflatex -interaction=nonstopmode panel_with_pairplot.tex')
+    os.system('rm panel_with_pairplot.log panel_with_pairplot.aux')
+
+for path in paths:
+    inner_paths = glob.glob(path+'/*/')
+    if len(inner_paths) > 0:
+        for inner_path in inner_paths:
+            create_file(inner_path)
+    else:
+        create_file(path)
